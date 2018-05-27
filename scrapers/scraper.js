e7c403fa5eef92eeb50e992ca51f0d78011f8af8
aashish-ak <kumaraashish118@gmail.com>
2018-05-28 00:34:10 +0530

    IITD scraped

--- scrapers/scraper.js
+++ scrapers/scraper.js
@@ -3,6 +3,8 @@ var cheerio = require('cheerio');
 
 var options = {
     url: 'http://www.cse.iitd.ernet.in/index.php/2011-12-29-23-14-30/faculty',
+    method: 'GET',
+    proxy: 'http://10.8.0.1:8080',
     headers: {
         'Connection': 'keep-alive',
         'Accept': '*/*',
@@ -10,25 +12,38 @@ var options = {
     }
 };
 
-function callback(error, response, html) {
+request(options, function (error, response, html) {
     if (!error && response.statusCode == 200) {
-        
+
         var $ = cheerio.load(html);
-        var fac = $('div.item-page').children('table').children('tbody').children('tr').filter(function(i, el){
+        var fac = $('div.item-page').children('table').children('tbody').children('tr').filter(function (i, el) {
             var name = $(this).children().find('a').text().trim();
             return name != "" && name != '\n';
         });
 
         fac.each(function (i, el) {
             var name = $(this).children().find('a').text();
-            var desig = $(this).children().find('a').parent().text().trim();
-            console.log(desig);
-        })
+            var work_area = $(this).children().find('a').parent().siblings('p').text().trim();
+            var email = $(this).children('td.pic').eq(2).children('p').eq(0).text().replace(" AT ", "@").replace(" ", "").replace("E-mail:", "").replace("Email:", "").trim();
+            var phone = $(this).children('td.pic').eq(2).children('p').eq(1).text().trim();
+            var image = $(this).children('td.pic').find('img').attr('src');
+            var link = $(this).children().find('a').attr('href');
+            if (image.indexOf("http://www.cse.iitd") < 0) {
+                image = "http://www.cse.iitd.ernet.in" + image;
+            }
+            var iitd_fac = {
+                image: image,
+                name: name,
+                work_area: work_area,
+                email: email,
+                phone: phone,
+                link: link
+            }
+            console.log(iitd_fac)
+        });
     }
     else {
         console.log(response.statusCode)
         console.log(error)
     }
-}
-
-request(options, callback);
+});
