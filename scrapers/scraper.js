b562a37d104adb8342df5253f31f083173064d3d
aashish-ak <kumaraashish118@gmail.com>
2018-05-27 16:49:26 +0530

    Project Initialised

--- scrapers/scraper.js
+++ scrapers/scraper.js
@@ -1,13 +1,34 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-request('https://www.iitmandi.ac.in', function (error, response, html) {
-    if (!error && response.statusCode == 200) {
-        var $ = cheerio.load(html);
-        console.log('FuckOU');
+var options = {
+    url: 'http://www.cse.iitd.ernet.in/index.php/2011-12-29-23-14-30/faculty',
+    headers: {
+        'Connection': 'keep-alive',
+        'Accept': '*/*',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36'
     }
-    else{
+};
+
+function callback(error, response, html) {
+    if (!error && response.statusCode == 200) {
+        
+        var $ = cheerio.load(html);
+        var fac = $('div.item-page').children('table').children('tbody').children('tr').filter(function(i, el){
+            var name = $(this).children().find('a').text().trim();
+            return name != "" && name != '\n';
+        });
+
+        fac.each(function (i, el) {
+            var name = $(this).children().find('a').text();
+            var desig = $(this).children().find('a').parent().text().trim();
+            console.log(desig);
+        })
+    }
+    else {
+        console.log(response.statusCode)
         console.log(error)
     }
-    // console.log(response.statusCode);
-})
+}
+
+request(options, callback);
