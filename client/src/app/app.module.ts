980e0f3d5dfa28d43916b0001bf48feb3598846e
aashish-ak <kumaraashish118@gmail.com>
2018-05-29 01:28:06 +0530

    Angular Routing DOne

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from "@angular/http";
+import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
-
+import { FilterPipe } from './filter.pipe';
+import { AboutComponent } from './about/about.component';
+import { MainComponent } from './main/main.component';
+import { AppRoutingModule } from './app-routing.module';
+import { APP_BASE_HREF } from "@angular/common";
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipe,
+    AboutComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    AppRoutingModule,
+    RouterModule
   ],
-  providers: [],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
