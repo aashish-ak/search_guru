22c22c93a83db90980941ee7fb742aaa80f4c37e
aashish-ak <kumaraashish118@gmail.com>
2018-05-29 14:50:06 +0530

    Angular Searching Pipe Working

--- client/src/app/filter.pipe.ts
+++ client/src/app/filter.pipe.ts
@@ -7,17 +7,24 @@ export class FilterPipe implements PipeTransform {
 
   transform(data: any[], searchIIT: string, searchText: string): any[] {
     if(!data) return [];
-    
     if(!searchText) return data;
+    searchText = searchText.toUpperCase();
+    searchIIT = searchIIT.toUpperCase();
+    if(searchIIT==="" || searchIIT ==="ALL IIT"){
+      return data.filter(item => {
+        return item.name.toUpperCase().includes(searchText)
+      })
+    }
     if(searchIIT){
       console.log(searchIIT);
       data = data.filter(item => {
         return item.collegeName === searchIIT;
       });
       return data.filter(item=>{
-        return item.name.toUpperCase().includes(searchText.toUpperCase())
+        return item.name.toUpperCase().includes(searchText);
       })
     }
+  
 
     // return data.filter(item =>{
     //   return item.toUpperCase().includes(searchText.toUpperCase());
