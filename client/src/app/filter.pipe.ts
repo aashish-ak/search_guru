f198df46d9cceb8cef32205b9681ecda204bd139
Dheeraj <dhirajyadav135@gmail.com>
2018-12-14 20:53:06 +0530

    Blank name shows result from selected iit

--- client/src/app/filter.pipe.ts
+++ client/src/app/filter.pipe.ts
@@ -7,8 +7,20 @@ export class FilterPipe implements PipeTransform {
 
   transform(data: any[], searchIIT: string, searchText: string): any[] {
     if(!data) return [];
-    if(!searchText) return data;
-    if(!searchIIT) return data;
+    if(!searchIIT&&!searchText) return data;
+    if(!searchIIT) searchIIT="ALL IIT";
+    if(!searchText)
+    {
+      searchIIT = searchIIT.toUpperCase();
+      if(searchIIT ==="ALL IIT"){
+        return data;
+      }
+      
+      data = data.filter(item => {
+        return item.collegeName.toUpperCase() === searchIIT;
+      });
+      return data;
+    }
     searchText = searchText.toUpperCase();
     searchIIT = searchIIT.toUpperCase();
     if(searchIIT ==="ALL IIT"){
